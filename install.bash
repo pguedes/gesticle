7ad281066e77614a6a8f56cd37eef3a40fedab28
Pedro Guedes <guedes.emigra@gmail.com>
2021-12-31 10:06:52 +0000

    fix autostart .desktop so that it does not block

---
+++ install.bash
@@ -0,0 +1,14 @@
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/pguedes/gesticle/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+version=$(get_latest_release)
+number="${version:1}"
+
+echo "installing https://github.com/pguedes/gesticle/releases/download/${version}/gesticled_${number}_amd64.deb"
+wget "https://github.com/pguedes/gesticle/releases/download/${version}/gesticled_${number}_amd64.deb" -P /tmp
+
+echo sudo apt install "/tmp/gesticled_${number}_amd64.deb"
